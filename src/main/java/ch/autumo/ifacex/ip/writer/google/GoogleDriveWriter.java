803eada7bafb8f9f484cb387dba34ac54d67c4c5
Michael Gasche <autumo.switzerland@gmail.com>
2023-03-03 06:36:58 +0100

    Fixes GoogleDriveWriter (update or create!)

--- src/main/java/ch/autumo/ifacex/ip/writer/google/GoogleDriveWriter.java
+++ src/main/java/ch/autumo/ifacex/ip/writer/google/GoogleDriveWriter.java
@@ -21,6 +21,7 @@ package ch.autumo.ifacex.ip.writer.google;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -29,6 +30,7 @@ import org.slf4j.LoggerFactory;
 import com.google.api.client.http.AbstractInputStreamContent;
 import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 import ch.autumo.ifacex.IPC;
 import ch.autumo.ifacex.IfaceXException;
@@ -43,6 +45,8 @@ import ch.autumo.ifacex.writer.Writer;
  * Google Drive file out - writer prefix 'gdrive_file_out'.
  * 
  * Writes files to a google drive (with or without a folder specified).
+ * It updates or creates new files, by determining existing matching
+ * folder names and file names!
  */
 public class GoogleDriveWriter extends AbstractGoogleDrive implements Writer {
 
@@ -61,19 +65,51 @@ public class GoogleDriveWriter extends AbstractGoogleDrive implements Writer {
 		folderIdParent = config.getWriterConfig(rwName).getConfig("_folder_id_parent");
 		
 		if (folder != null && folder.length() > 0) {
-			final File fileMetadata = new File();
-	        fileMetadata.setName(folder);
-	        fileMetadata.setMimeType("application/vnd.google-apps.folder");
-			if (folderIdParent != null && folderIdParent.length() > 0) {
-	            final List<String> parents = Arrays.asList(folderIdParent);
-	            fileMetadata.setParents(parents);
-	        }
-			
-			//TODO: check if folder with same name already exists!
-			
+	        
 			try {
-				final File fld = service().files().create(fileMetadata).setFields("id,name").execute();
-				folderId = fld.getId();
+				
+				final FileList existingFolders = service().files().list().setQ( "mimeType='application/vnd.google-apps.folder' and name='"+folder+"' and trashed=false").execute();
+				
+				FIND: for (Iterator<File> iterator = existingFolders.getFiles().iterator(); iterator.hasNext();) {
+					
+					final File curr = iterator.next();
+					
+					if (folderIdParent != null && folderIdParent.length() > 0) {
+						
+						final List<String> parents = curr.getParents();
+						
+						for (Iterator<String> iterator2 = parents.iterator(); iterator2.hasNext();) {
+							final String pid = iterator2.next();
+							if (pid.equals(folderIdParent) && curr.getName().equals(folder)) {
+								folderId = curr.getId();
+								break FIND;
+							}
+						}
+						
+					} else {
+						
+						if (curr.getName().equals(folder)) {
+							// First match!
+							folderId = curr.getId();
+							break FIND;
+						}
+					}
+				}
+				
+				if (folderId == null) { // still not found
+				
+					final File fileMetadata = new File();
+			        fileMetadata.setName(folder);
+			        fileMetadata.setMimeType("application/vnd.google-apps.folder");
+					
+					if (folderIdParent != null && folderIdParent.length() > 0) {
+			            final List<String> parents = Arrays.asList(folderIdParent);
+			            fileMetadata.setParents(parents);
+			        }
+					final File fld = service().files().create(fileMetadata).setFields("id,name").execute();
+					folderId = fld.getId();
+				}
+			
 			} catch (IOException e) {
 				throw new IfaceXException("Couldn't create folder '"+folder+"'!", e);
 			}
@@ -111,9 +147,27 @@ public class GoogleDriveWriter extends AbstractGoogleDrive implements Writer {
 	        
 	        final AbstractInputStreamContent uploadStreamContent = new FileContent(null, curr);
 	        File file = null;
+	        
 	        try {
-				file = service().files().create( fileMetadata, uploadStreamContent).setFields("id,name").execute();
-				//file = service().files().create( fileMetadata, uploadStreamContent).setFields("id,name,webContentLink,webViewLink,parents").execute();
+	        	
+	        	FileList existingFiles = null;
+	        	if (fid != null)
+	        		existingFiles = service().files().list().setQ( "parents in '"+fid+"' and name='"+curr.getName()+"' and trashed=false").execute();
+	        	else
+	        		existingFiles = service().files().list().setQ( "name='"+curr.getName()+"' and trashed=false").execute();
+	        	
+	        	final List<File> files = existingFiles.getFiles();
+	        	if (files.size() > 0) {
+	        		// UPDATE
+	        		fileMetadata.setParents(null); // if we have a specific file id, it is not allowed to have a parent set!
+	        		file = service().files().update( files.get(0).getId(), fileMetadata, uploadStreamContent).setFields("id,name").execute();
+	        	} else {
+	        		// CREATE
+	        		file = service().files().create( fileMetadata, uploadStreamContent).setFields("id,name").execute();
+	        	}
+	        	
+				// Further fields, e.g., "id,name,webContentLink,webViewLink,parents"
+	        	
 			} catch (IOException e) {
 				throw new IfaceXException("Couldn't upload file '"+curr.getName()+"'!", e);
 			}
